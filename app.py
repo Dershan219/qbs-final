8027a71e91ed8ecdab67279237326c42603c8567
Andy Chen <andy27479@gmail.com>
2020-06-05 15:56:12 +0800

    First Modification

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ import sqlite3
 import time
 import os
 
-os.chdir('C:/Users/Andy/Desktop/NTU Courses/Quantitative Business Science/Project')
-
 app = dash.Dash(__name__)
+
+external_css = ["https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"]
+for css in external_css:
+    app.css.append_css({"external_url": css})
 
 app.layout = html.Div(children=[
     html.H2("Live Twitter Sentiment"),
@@ -22,15 +24,18 @@ app.layout = html.Div(children=[
 @app.callback(
     Output('live-graph', 'figure'),
     [Input('keyword', 'value'),
-    Input('graph-update', 'n_intervals')]
+     Input('graph-update', 'n_intervals')]
 )
 
 def update_graph(keyword, n):
     conn = sqlite3.connect('twitter.db')
-    df = pd.read_sql('SELECT * FROM tweets WHERE tweet LIKE ? ORDER BY time DESC LIMIT 200', conn, params=('%' + keyword + '%', ))
+    df = pd.read_sql(
+        'SELECT * FROM tweets WHERE tweet LIKE ? ORDER BY time DESC LIMIT 1000',
+        conn, params=('%' + keyword + '%', ))
     df['sentiment_smoothed'] = df['sentiment'].rolling(int(len(df)/5)).mean()
     df['date'] = pd.to_datetime(df['time'], unit='ms')
     df.set_index('date', inplace=True)
+    df = df.resample('10s').mean()
     df.dropna(inplace=True)
 
     X = df.index
@@ -45,7 +50,7 @@ def update_graph(keyword, n):
 
     return {'data':[data], 'layout':go.Layout(xaxis=dict(range=[min(X), max(X)]),
                                               yaxis=dict(range=[min(Y), max(Y)]),
-                                              title='Term: {}'.format(keyword))}
+                                              title='Keyword: {}'.format(keyword))}
 
 if __name__ == '__main__':
     app.run_server(debug=True)
