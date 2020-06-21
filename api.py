f65ab812e34b4483c2e3be6ef6c3fb371c1f7535
Andy Chen <andy27479@gmail.com>
2020-06-21 19:18:12 +0800

    added dbc components

--- api.py
+++ api.py
@@ -5,9 +5,7 @@ from datetime import datetime, timedelta
 import time
 from unidecode import unidecode
 import json
-from pandas.io.json import json_normalize
 import sqlite3
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
 from keras import backend as K
@@ -24,12 +22,7 @@ consumer_secret = "QBfEpERH8d6IdVCPBs4SQaLWfiXgG2b3KRGIV8QrJrBUNJWF2k"
 access_token = "712293623665045505-c2NWLT2AQqLVRRS8jcRKxyyHsQ1kpG6"
 access_secret = "o82dQvo1DEL2q4Cnp9EehhxsWUW8hs0huV5KwL2tX3dAs"
 #%%
-# Creating Tweets Database-------------------------------------------------
-conn = sqlite3.connect('twitter.db')
-c = conn.cursor()
-
-analyzer = SentimentIntensityAnalyzer()
-
+# preparing LSTM model-----------------------------------------------------
 def jaccard_distance_loss(y_true, y_pred, smooth=100):
     intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
     sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
@@ -42,43 +35,62 @@ def jaccard_coef(y_true, y_pred, smooth=1e-12):
     jac = (intersection + smooth) / (sum_ - intersection + smooth)
     return K.mean(jac)
 
-model = load_model('train_model_LSTM.h5', custom_objects={'jaccard_distance_loss':jaccard_distance_loss, 'jaccard_coef':jaccard_coef})
+model = load_model(
+    'train_model_LSTM.h5',
+    custom_objects={
+        'jaccard_distance_loss':jaccard_distance_loss,
+        'jaccard_coef':jaccard_coef})
 
 with open('tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
 stop_words = set(stopwords.words('english'))
+stop_words.add('rt')
 stop_words.remove('not')
 
 def preprocess(text):
-    review=re.sub('@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+',' ',text)
+    review=re.sub(r"RT |@\S+ |https:\S+|http:\S+|[^a-zA-Z0-9' ]",' ',text)
     review=review.lower()
     review=review.split()
     review=[word for word in review if not word in stop_words]
     review_tokenized=pad_sequences(tokenizer.texts_to_sequences([review]), maxlen=300)
     return review, review_tokenized
 
+# Creating Tweets Database-------------------------------------------------
+conn = sqlite3.connect('twitter.db')
+c = conn.cursor()
 # create table for tweets
 def create_table():
     c.execute("CREATE TABLE IF NOT EXISTS tweets (id TEXT PRIMARY KEY, time INTEGER, tweet TEXT, sentiment REAL, keywords TEXT)")
     conn.commit()
-
 create_table()
 
 # update tweets with stream listener
 class Listener(StreamListener):
+    def __init__(self):
+        self.cnt = 0
+
     def on_data(self, data):
         try:
+            self.cnt += 1
             tweet_data = json.loads(data)
             tweet_id = str(tweet_data['id_str'])
-            tweet = unidecode(tweet_data['text'])
+            try:
+                tweet = unidecode(tweet_data['extended_tweet']['full_text'])
+            except Exception as e:
+                tweet = unidecode(tweet_data['text'])
             time_ts = tweet_data['timestamp_ms']
+            time_dt = tweet_data['created_at']
             # sentiment = analyzer.polarity_scores(tweet)['compound'] # calculate sentiment scores
             keywords, x_test = preprocess(tweet)
             sentiment = float(model.predict(x_test, batch_size=1024)[0][0])
             keywords = ', '.join(keywords)
-            print(sentiment, type(sentiment))
-            c.execute("INSERT OR IGNORE INTO tweets (id, time, tweet, sentiment, keywords) VALUES (?, ?, ?, ?, ?)", 
+            print("Writing Tweet # {}".format(self.cnt))
+            print("Tweet Created at {}".format(time_dt))
+            print(tweet)
+            print("Tweet Sentiment: {}".format(sentiment))
+            c.execute(
+                "INSERT OR IGNORE INTO tweets (id, time, tweet, sentiment, keywords) VALUES (?, ?, ?, ?, ?)", 
                 (tweet_id, time_ts, tweet, sentiment, keywords))
             conn.commit()
         except KeyError as e:
@@ -94,24 +106,18 @@ while True:
     try:
         auth = OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_secret)
-        twitter_stream = Stream(auth, Listener())
-        twitter_stream.filter(track=["a","e","i","o","u"])
+        twitter_stream = Stream(auth, Listener(), tweet_mode='extended')
+        twitter_stream.filter(languages=["en"], track=["a","e","i","o","u"])
     except Exception as e:
         print(str(e))
         time.sleep(5)
 #%%
-# Utility Functions--------------------------------------------------------
-# query tweets with keyword for database
-def query_table(keyword):
-    df = pd.read_sql('SELECT * FROM tweets WHERE tweet LIKE ? ORDER BY time DESC LIMIT 200', conn, params=('%' + keyword + '%', ))
-    df['date'] = pd.to_datetime(df['time'], unit='ms')
-    return df
-
+# utility functions--------------------------------------------------------
 # check out top 15 popular tweets
-def popular_tweets(keyword, days=7): # yesterday → days = 1
-    api = API(auth)
-    until = datetime.now().date()-timedelta(days=days)
-    results = api.search(keyword, until=until)
-    results_df = json_normalize([r._json for r in results])
-    tweets_df = results_df.iloc[:,[0,2,3]]
-    return tweets_df
+# def popular_tweets(keyword, days=7): # yesterday → days = 1
+#     api = API(auth)
+#     until = datetime.now().date()-timedelta(days=days)
+#     results = api.search(keyword, until=until)
+#     results_df = json_normalize([r._json for r in results])
+#     tweets_df = results_df.iloc[:,[0,2,3]]
+#     return tweets_df
--- app.py
+++ app.py
@@ -2,26 +2,126 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+import dash_table
 import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 import pandas as pd
 import sqlite3
 import time
 import os
+import re
 
 os.chdir('C://Users/Andy/Desktop/NTU Courses/Quantitative Business Science/Project/twitter-sentiment')
 
-app = dash.Dash(__name__)
+# global settings-----------------------------------------------------------
 
-external_css = ["https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"]
-for css in external_css:
-    app.css.append_css({"external_url": css})
+app_colors = {
+    'background':'#272B30',
+    'text':'#AAAAAA',
+    'plot1':'#FFFFFF',
+    'plot2':'#B54057',
+    'plot3':'#5C5C5C'
+}
 
-app.layout = html.Div(children=[
-    html.H2("Live Twitter Sentiment"),
-    dcc.Input(id='keyword', value='Trump', type='text'),
-    dcc.Graph(id='live-graph', animate=False),
-    dcc.Interval(id='graph-update', interval=1*1000)
-    ])
+sentiment_colors = {0:"#FFE6AC",
+                    0.5:"#D0F2DF",
+                    1:"#9CEC5B"}
+
+LOGO = "https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
+THEME = dbc.themes.SLATE
+REFRESH = 1000 # ms
+
+app = dash.Dash(__name__,
+                external_stylesheets=[THEME])
+
+#%%
+# layout settings-----------------------------------------------------------
+header = dbc.Navbar(
+    [
+        dbc.Col(html.Img(src=LOGO, height="30px"), md=0.5),
+        dbc.Col(
+            dbc.NavbarBrand(
+                "Twitter Sentiment",
+                href="https://github.com/Dershan219/twitter-sentiment"),
+                md=2
+                ),
+        dbc.Col(
+            dbc.Input(
+                id='keyword', value='Trump',
+                type='text', placeholder="Search Term"),
+                md=9
+                ),
+        dbc.Col(
+            dbc.Button("Search", color="primary"),
+            md=0.5
+            )
+    ],
+    color="dark",
+    dark=True,
+)
+
+body = dbc.Container(
+    [
+        html.Br(),
+        dbc.Row(
+            [
+                dbc.Col(html.Button('Loading related terms...', id='live-term'), width=6),
+                dcc.Interval(id='term-update', interval=10*REFRESH)
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(dcc.Graph(id='live-graph', animate=False), width=12),
+                dcc.Interval(id='graph-update', interval=REFRESH)
+            ],
+            justify='center'
+        ),
+        html.Br(),
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.H5(
+                        "Most Negative Tweets",
+                        style={'text-align':'center', 'color':'{}'.format(app_colors['text'])}
+                        ),
+                    width=6),
+                dbc.Col(
+                    html.H5(
+                        "P/N Ratio",
+                        style={'text-align':'center', 'color':'{}'.format(app_colors['text'])}
+                        ),
+                    width=6),               
+            ],
+            justify='between'
+        ),
+        html.Br(style={'margin-bottom':'2px'}),
+        dbc.Row(
+            [
+                dbc.Col(html.Div(id='live-table'), width=6),
+                dcc.Interval(id='table-update', interval=REFRESH),
+                dbc.Col(dcc.Graph(id='live-pie', animate=False), width=6),
+                dcc.Interval(id='pie-update', interval=10*REFRESH),
+            ],
+            justify='between'
+        )
+    ]
+)
+
+#%%
+# sentiment graph-----------------------------------------------------------
+
+# def generate_terms(df):
+
+
+# @app.callback(
+#     Output('live-term', 'children'),
+#     [Input('keyword', 'value'),
+#      Input('term-update', 'n_intervals')]
+# )
+
+# def update_terms(keyword, n):
+
 
 @app.callback(
     Output('live-graph', 'figure'),
@@ -32,27 +132,164 @@ app.layout = html.Div(children=[
 def update_graph(keyword, n):
     conn = sqlite3.connect('twitter.db')
     df = pd.read_sql(
-        'SELECT * FROM tweets WHERE tweet LIKE ? ORDER BY time DESC LIMIT 1000',
+        'SELECT * FROM tweets WHERE tweet LIKE ? ORDER BY time DESC LIMIT 2000',
         conn, params=('%' + keyword + '%', ))
+    
     df['sentiment_smoothed'] = df['sentiment'].rolling(int(len(df)/5)).mean()
-    df['date'] = pd.to_datetime(df['time'], unit='ms')
+    df['date'] = pd.to_datetime((df['time']/1000).astype(int), unit='s')
     df.set_index('date', inplace=True)
-    df = df.resample('10s').mean()
-    df.dropna(inplace=True)
+    
+    df = df.resample('30s').agg({"id":'size',"sentiment_smoothed":'mean'})
+    df['id'].fillna(0, inplace=True)
+    df['sentiment_smoothed'].fillna(method='ffill', inplace=True)
 
     X = df.index
-    Y = df.sentiment_smoothed
+    Y1 = df.sentiment_smoothed.values.round(2)
+    Y2 = df.id.values
 
     data = go.Scatter(
         x=X,
-        y=Y,
+        y=Y1,
         name='Scatter',
-        mode='lines+markers'
+        mode='lines',
+        line=dict(color=app_colors["plot1"], width=3),
+        yaxis='y')
+
+    data2 = go.Bar(
+        x=X,
+        y=Y2,
+        name='Bar',
+        marker=dict(color=app_colors["text"], opacity=0.8),
+        yaxis='y2')
+
+    layout = go.Layout(
+        xaxis=dict(
+            range=[min(X), max(X)],
+            color=app_colors["text"],
+            showspikes=True),
+        yaxis=dict(
+            range=[min(Y1),max(Y1)],
+            title='sentiment', side='left', overlaying='y2',
+            color=app_colors["text"],
+            showgrid=False, showspikes=True),
+        yaxis2=dict(
+            range=[min(Y2),max(Y2*4)],
+            title='volume', side='right',
+            color=app_colors["text"],
+            showgrid=False, showspikes=True),
+        showlegend=False,
+        plot_bgcolor=app_colors["background"],
+        paper_bgcolor=app_colors["background"],
+        title=dict(
+            text='Keyword: {}'.format(keyword),
+            font=dict(color=app_colors["text"]),
+            x=0.5,
+            y=0.9,
+            xanchor='center',
+            yanchor='top'),
+        margin=go.layout.Margin(
+            l=50,
+            r=50,
+            pad=15
+        ))
+
+    return go.Figure(
+        data=[data, data2], 
+        layout=layout)
+
+#%%
+#most negative tweets-------------------------------------------------------
+def generate_table(df):
+    return dash_table.DataTable(
+        id='live-table',
+        columns=[{"name": i, "id": i, "selectable":True} for i in df.columns],
+        data=df.to_dict('records'),
+        filter_action="native",
+        sort_action="native",
+        style_header={
+            'backgroundColor':app_colors["plot3"],
+            'color':app_colors["plot1"],
+            'fontWeight': 'bold',
+            'textAlign': 'center',
+            'border-bottom': '2px solid {}'.format(app_colors["background"])},
+        style_cell={
+            'backgroundColor':app_colors["text"],
+            'color':app_colors["background"], 'height': 'auto',
+            'textAlign': 'center',
+            'border': '1px solid {}'.format(app_colors["background"])},
+        style_cell_conditional=[
+            {'if': {'column_id': 'tweet'},
+            'width': '480px',
+            'minWidth': '480px',
+            'maxWidth': '480px',
+            'whiteSpace': 'normal',
+            'textAlign': 'left'
+            }],
+        page_size=5,
+        css=[
+            {'selector': '.previous-page, .next-page, .last-page, .first-page',
+            'rule': 'background-color: #272B30;'}
+            ]
+        )
+
+@app.callback(
+    Output('live-table', 'children'),
+    [Input('keyword', 'value'),
+     Input('table-update', 'n_intervals')]
+)
+
+def update_negative_tweets(keyword, n):
+    conn = sqlite3.connect('twitter.db')
+    df = pd.read_sql(
+        'SELECT tweet, sentiment FROM tweets WHERE tweet LIKE ? ORDER BY sentiment LIMIT 30',
+        conn, params=('%' + keyword + '%', )
+    )
+    df['tweet'] = df['tweet'].apply(lambda x: re.sub(r"RT|@\S+|\Whttps:\S+|\Whttp:\S+|\.\.\.",' ', x))
+    df['sentiment'] = df['sentiment'].apply(lambda x: re.sub("(?<=....)(.*?)(?=e.+)", '', str(x))).astype(float)
+    return generate_table(df)
+
+#%%
+# P/N Ratio
+@app.callback(
+    Output('live-pie', 'figure'),
+    [Input('keyword', 'value'),
+     Input('pie-update', 'n_intervals')]
+)
+
+def update_pie(keyword, n):
+    conn = sqlite3.connect('twitter.db')
+    df = pd.read_sql(
+        'SELECT * FROM tweets WHERE tweet LIKE ? ORDER BY time DESC LIMIT 2000',
+        conn, params=('%' + keyword + '%', ))
+
+    df['sentiment_binary'] = df['sentiment'].apply(lambda x: 'negative' if x<0.5 else 'positive')
+
+    sentiment = df['sentiment_binary'].value_counts().values
+
+    data = go.Pie(
+        labels=['positive', 'negative'],
+        values=sentiment,
+        textinfo='label+percent',
+        marker=dict(colors=[app_colors['text'], app_colors['plot2']])
     )
 
-    return {'data':[data], 'layout':go.Layout(xaxis=dict(range=[min(X), max(X)]),
-                                              yaxis=dict(range=[min(Y), max(Y)]),
-                                              title='Keyword: {}'.format(keyword))}
+    layout = go.Layout(
+        showlegend=False,
+        plot_bgcolor=app_colors["background"],
+        paper_bgcolor=app_colors["background"],
+        height=280,
+        margin=go.layout.Margin(
+            t=0,
+            b=0
+        )
+    )
+
+    return go.Figure(
+        data=data,
+        layout=layout
+    )
+
+app.layout = html.Div([header, body])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
